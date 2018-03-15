b4d94e68f29ce2453341f9af91262166348ee1e3
EmeryP <emeryparks09@hotmail.com>
2018-03-14 17:02:15 -0700

    form will updated table

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 'use strict';
 
 //create empty array to push each store info to
-var dailyTotals = [];
+var storesArray = []; //all stores becuase hold stores
 
 //global var with hours open
-var hoursOpen = ['', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 // we need to access the table that is in the DOM
 var cookieTable = document.getElementById('cookies');
+
+//access the form so we can attach an event listener
+var storeForm = document.getElementById('storeForm');
 
 // =====================================================
 
 // create function to push hourOpen into th
 function cookieStandHours(){
+
+  var tdElement = document.createElement('td');
+  tdElement.textContent = '';
 
   //use for loop to push hours array to table header
   for (var i = 0; i < hoursOpen.length; i++) {
@@ -36,7 +42,7 @@ cookieStandHours();
 
 // ======================================================
 //create constructor object
-function CookieStandSales(minCustPerHour, maxCustPerHour, avgCookiePerSale, location){
+function CookieStand(minCustPerHour, maxCustPerHour, avgCookiePerSale, location){
 
   // create all dynamic properties
   this.minCustPerHour = minCustPerHour;
@@ -48,14 +54,14 @@ function CookieStandSales(minCustPerHour, maxCustPerHour, avgCookiePerSale, loca
   //create day totals
   this.dailySalesTotals = 0;
   // push this info to empty array
-  dailyTotals.push(this);
-  console.log(dailyTotals);
+  storesArray.push(this);
+  // console.log(storesArray);
 }
 
 // =================================================================
 
 //creating function to generate random num and push to empty array
-CookieStandSales.prototype.salesFiguresGenerator = function (){
+CookieStand.prototype.salesFiguresGenerator = function (){
 
   for (var i = 0; i < hoursOpen.length - 1; i++) {
 
@@ -71,7 +77,7 @@ CookieStandSales.prototype.salesFiguresGenerator = function (){
 };
 // ================================================================
 
-CookieStandSales.prototype.render = function (){
+CookieStand.prototype.render = function (){
 
   this.salesFiguresGenerator();
 
@@ -101,74 +107,82 @@ CookieStandSales.prototype.render = function (){
 };
 
 // =======================================================================
-// Total footer
-// CookieStandSales.prototype.render = function (){
-//   function hourlyTotals(){
-
-//     //use for loop to push hours array to table header
-//     for (var i = 0; i < hoursOpen.length; i++) {
-
-//       //create <th>
-//       var tfElement = document.createElement('tf');
-//       // var tdElement = document.createElement('td');
-
-//       //create th content
-//       tfElement.textContent = 'totals'+ '';
-
-//       //append th to table in DOM
-//       cookieTable.appendChild(tfElement);
-//     }
-//     // repeat code from above with new statement to create daily totals column
-//     // thElement = document.createElement('tr');
-//     // thElement.textContent = '';
-//     // cookieTable.appendChild(trElement);
-//   }
-// // };
-// // call hourlytotals
-// hourlyTotals();
-
-// create function to push hourOpen into th
+// create function to push total hourly sales into the footer row
 function cookieStandHourlyTotals(){
 
-// CookieStandSales.prototype.render = function (){
+  var trElement = document.createElement('tr');
+  var tdElement = document.createElement('td');
+  tdElement.textContent = 'Total';
+  trElement.appendChild(tdElement);
 
-//use for loop to push hours array to table header
+  //use for loop to push hours array to table header
   for (var i = 0; i < hoursOpen.length; i++) {
 
-  //create <th>
-    var trElement = document.createElement('tr');
+    var hourlyCounter = 0;
 
-    // var tdElement = document.createElement('td');
-    var tfElement = document.createElement('tfoot');
+    //nested for loop used iterate through stores array and add value at stores position
+    for (var store = 0; store < storesArray.length; store++) {
 
-    //create th content
-    tfElement.textContent = 'totals' + '';
-    trElement.appendChild(tfElement);
+      hourlyCounter += storesArray[store].allCookiesStandSales[i];
+    }
 
-  //append th to table in DOM
+    tdElement = document.createElement('td');
 
+    //create the content
+    tdElement.textContent = hourlyCounter;
+
+    trElement.appendChild(tdElement);
   }
-
-  cookieTable.appendChild(tfElement);
+  //append th to table in DOM
+  cookieTable.appendChild(trElement);
 }
 // call cookieStandHours func
-cookieStandHourlyTotals();
+
 
 // =======================================================================
 
 //create var with new location instance for each store
-var firstAndPike = new CookieStandSales(23, 65, 6.3, 'First and Pike');
-var seatacAirport = new CookieStandSales(3, 24, 1.2, 'SeaTac Airport');
-var seattleCenter = new CookieStandSales(11, 38, 3.7, 'Seattle Center');
-var capitolHill = new CookieStandSales(20, 38, 2.3, 'Capitol Hill');
-var alki = new CookieStandSales(2, 16, 4.6, 'Alki');
+var firstAndPike = new CookieStand(23, 65, 6.3, 'First and Pike');
+var seatacAirport = new CookieStand(3, 24, 1.2, 'SeaTac Airport');
+var seattleCenter = new CookieStand(11, 38, 3.7, 'Seattle Center');
+var capitolHill = new CookieStand(20, 38, 2.3, 'Capitol Hill');
+var alki = new CookieStand(2, 16, 4.6, 'Alki');
 
 // call the render method on each var
-firstAndPike.render();
-seatacAirport.render();
-seattleCenter.render();
-capitolHill.render();
-alki.render();
+// firstAndPike.render();
+// seatacAirport.render();
+// seattleCenter.render();
+// capitolHill.render();
+// alki.render();
+// cookieStandHourlyTotals();
 
+// ================================================================
 
+function renderAllStores(){
+  for(var i in storesArray){
+    storesArray[i].render();
+  }
+}
 
+//event listeners call back function
+function addNewStore(event){
+  event.preventDefault();
+  // console.log(event.target.minCustPerHour.value);
+
+  //assigning new value to property assigned to current property
+  var newMinCustPerHour = event.target.minCustPerHour.value;
+  var newMaxCustPerHour = event.target.maxCustPerHour.value;
+  var newAvgCookiePerSale = event.target.avgCookiePerSale.value;
+  var newLocation = event.target.location.value;
+
+  new CookieStand(newMinCustPerHour, newMaxCustPerHour, newAvgCookiePerSale, newLocation);
+
+  cookieTable.innerHTML = '';
+  renderAllStores();
+}
+
+//add event listener, listening for event
+storeForm.addEventListener('submit', addNewStore);
+
+//Now we need to call our functions
+renderAllStores();
